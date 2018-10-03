fa530587dadfaa38d5d19d3b4030ff239ae9c407
mreyrosa <40677696+mreyrosa@users.noreply.github.com>
2018-10-03 00:21:16 -0700

    added navbar.js for solid transition. Added content to middle of pictures. Added pictures. Added Logo. Changes to site.css

--- lendmyautoinfo/lendmyautoinfo/App_Start/BundleConfig.cs
+++ lendmyautoinfo/lendmyautoinfo/App_Start/BundleConfig.cs
@@ -13,6 +13,8 @@ namespace lendmyautoinfo
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/navbar").Include(
+                        "~/Scripts/navbar.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
